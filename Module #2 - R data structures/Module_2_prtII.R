b13edfb612306e8ed899596b674040d267fe6587
Henry Miller <millerh1@livemail.uthscsa.edu>
2021-03-22 16:44:55 -0500

    Added mod 3

---
+++ Module #2 - R data structures/Module_2_prtII.R
@@ -0,0 +1,17 @@
+# Discuss Data Frames
+View(iris)
+
+# Get row 1
+iris[1,]
+
+# Get column 1
+iris[,1]
+
+# How to get the value in row 1, column 2?
+
+# Access a column by name
+iris$Petal.Length
+
+# How would I get the "species" in row 3?
+
+
